af7c37e38263c2f65149b18312544151f3e54b9e
AvotraIsmael <avotraismael4@gmail.com>
2023-05-15 23:01:20 +0100

    add searc h and sort

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -5,11 +5,12 @@ import { asyncWrapper } from './../middleware/async_wrapper'
 interface QueryObject {
   featured?: boolean
   company?: string
+  name?: string
 }
 
 export const getAllProducts = asyncWrapper(
   async (req: Request, res: Response) => {
-    const { featured, company } = req.query
+    const { featured, company, name, sort } = req.query
 
     const queryObject: QueryObject = {}
     if (featured) {
@@ -20,16 +21,30 @@ export const getAllProducts = asyncWrapper(
       queryObject.company = company as string
     }
 
-    console.log(queryObject)
+    if (name) {
+      queryObject.name = {
+        $regex: name as string,
+        $options: 'i',
+      } as unknown as string
+    }
 
-    const products = await ProductModel.find(queryObject)
+    // console.log(queryObject)
+
+    let result = ProductModel.find(queryObject)
+    if (sort) {
+      console.log(sort)
+      const sortList = (sort as string).split(',').join(' ')
+      console.log(sortList)
+      result = result.sort(sortList)
+    } else {
+      result = result.sort('createdAt')
+    }
+    const products = await result
     res.status(200).json({ products, nbHits: products.length })
   }
 )
 
 export const getAllProductsStatic = async (req: Request, res: Response) => {
-  const products = await ProductModel.find({
-    name: 'vase table',
-  })
+  const products = await ProductModel.find({}).sort('-name price')
   res.status(200).json({ products, nbHits: products.length })
 }
