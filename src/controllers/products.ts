9088a2f25aa2810211e5176c428b5b078dc4e2db
AvotraIsmael <avotraismael4@gmail.com>
2023-05-17 00:49:06 +0100

    filter and sort functionality

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -8,9 +8,13 @@ interface QueryObject {
   name?: string
 }
 
+type OperatorMap = {
+  [key: string]: string
+}
+
 export const getAllProducts = asyncWrapper(
   async (req: Request, res: Response) => {
-    const { featured, company, name, sort } = req.query
+    const { featured, company, name, sort, fields, numericFilters } = req.query
 
     const queryObject: QueryObject = {}
     if (featured) {
@@ -28,23 +32,54 @@ export const getAllProducts = asyncWrapper(
       } as unknown as string
     }
 
+    if (numericFilters) {
+      const operatorMap: OperatorMap = {
+        '>': '$gt',
+        '>=': '$gte',
+        '=': '$eq',
+        '<': '$lt',
+        '<=': '$lte',
+      }
+      const regEx = /\b(<|>|>=|=|<|<=)\b/g
+      let filters = (numericFilters as string).replace(
+        regEx,
+        (match) => `-${operatorMap[match]}-`
+      )
+      console.log(filters)
+    }
+
     // console.log(queryObject)
 
     let result = ProductModel.find(queryObject)
+    // sort
     if (sort) {
-      console.log(sort)
       const sortList = (sort as string).split(',').join(' ')
-      console.log(sortList)
       result = result.sort(sortList)
     } else {
       result = result.sort('createdAt')
     }
+
+    if (fields) {
+      const fieldsList = (fields as string).split(',').join(' ')
+      result = result.select(fieldsList)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
+
     const products = await result
     res.status(200).json({ products, nbHits: products.length })
   }
 )
 
 export const getAllProductsStatic = async (req: Request, res: Response) => {
-  const products = await ProductModel.find({}).sort('-name price')
+  const products = await ProductModel.find({
+    price: { $gt: 30, $lt: 50 },
+  })
+    .sort('price')
+    .select('name price')
   res.status(200).json({ products, nbHits: products.length })
 }
